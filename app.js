2fa0d39288712033edba271bb501eac69dd80d17
Girverius <gsmac1224@gmail.com>
2020-12-12 11:26:41 -0600

    added logic and html generator

--- app.js
+++ app.js
@@ -4,12 +4,103 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employees = [];
+
+const askQuestions = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of the team member?",
+      },
+
+      {
+        type: "input",
+        name: "id",
+        message: "What is the id of the team member?",
+      },
+
+      {
+        type: "list",
+        name: "role",
+        message: "Choose a role.",
+        choices: ["Engineer", "Intern", "Manager"],
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Engineer please enter your github username.",
+        when: (response) => response.role === "Engineer",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Intern plese enter your school.",
+        when: (response) => response.role === "Intern",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Manager, please enter office number.",
+        when: (response) => response.role === "Manager",
+      },
+      {
+        type: "list",
+        name: "continue",
+        message: "Do you want to continue with more team members?",
+        choices: ["Yes", "No"],
+      },
+    ])
+
+    .then((answers) => {
+      //   console.log(answers);
+      if (answers.role === "Engineer") {
+        const engineer = new Engineer(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.github
+        );
+        employees.push(engineer);
+      } else if (answers.role === "Intern") {
+        const intern = new Intern(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.school
+        );
+        employees.push(intern);
+      } else {
+        const manager = new Manager(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.officeNumber
+        );
+        employees.push(manager);
+      }
+      if (answers.continue === "Yes") {
+        askQuestions();
+      } else {
+        const html = render(employees);
+        // console.log(html)
+        writeFileAsync(outputPath, html).then(() => {
+          console.log("written");
+        });
+      }
+    });
+};
+
+askQuestions();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
